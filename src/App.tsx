336f40090f57e973aba40caf205922a0cd909497
Enick Jhony <enick.jhony@gmail.com>
2023-06-20 09:52:34 -0400

    Criando o formulário de NewTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import LogoTodo from './assets/logo.svg'
+import { PlusCircle } from 'phosphor-react'
 import styles from './App.module.css'
 import './global.css'
 
@@ -8,6 +9,15 @@ export default function App() {
       <header className={styles.header}>
         <img src={LogoTodo} alt="Logo Ignite Todo" />
       </header>
+      <div className={styles.wrapper}>
+        <form className={styles.newTask}>
+          <textarea name="task" placeholder="Adicione uma nova tarefa" />
+          <button type="submit">
+            Criar
+            <PlusCircle size={20} />
+          </button>
+        </form>
+      </div>
     </main>
   )
 }
