0529f9c56f72d1fbd5883552878d6d297b6e06b9
Enick Jhony <enick.jhony@gmail.com>
2023-07-09 16:38:24 -0400

    Estilizando o Chekbox do `Card.tsx`

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
-import { Circle, Trash } from 'phosphor-react'
+import { CheckCircle, Circle, Trash } from 'phosphor-react'
 import styles from './Card.module.css'
+import { useState } from 'react'
 
 interface CardProps {
   content: string
@@ -9,7 +10,13 @@ interface CardProps {
   onCheckbox: (id: string) => void
 }
 
-export function Card({ content, id, onDeleteTask, onCheckbox }: CardProps) {
+export function Card({
+  content,
+  id,
+  isCompleted,
+  onDeleteTask,
+  onCheckbox
+}: CardProps) {
   function handleDeleteTask(id: string) {
     onDeleteTask(id)
   }
@@ -18,12 +25,31 @@ export function Card({ content, id, onDeleteTask, onCheckbox }: CardProps) {
     onCheckbox(id)
   }
 
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handleMouseEnter = () => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+  }
+
   return (
     <div className={styles.card}>
-      <button onClick={() => handleCheckbox(id)}>
-        <Circle size={20} />
+      <button
+        className={`${styles.checkbox} ${isHovered ? styles.hovered : ''}`}
+        onClick={() => handleCheckbox(id)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {isCompleted ? (
+          <CheckCircle className={styles.circleHovered} size={20} weight={isHovered ? 'fill' : 'fill'} />
+        ) : (
+          <Circle size={20} weight={isHovered ? 'duotone' : 'regular'} />
+        )}
       </button>
-      <input className={styles.checkbox} type="checkbox" />
+      {/* <input className={styles.checkbox} type="checkbox" /> */}
       <p>{content}</p>
       <button
         title="Deletar Task!"
